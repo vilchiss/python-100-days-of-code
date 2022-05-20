3394c76f115ff2b5a1e27d6f4512e17d02a67eec
Luis Oropeza <luis.oropeza.129@gmail.com>
2022-05-20 00:29:23 -0500

    FIX list index

--- day_4/README.md
+++ day_4/README.md
@@ -4,8 +4,8 @@ Piedra, papel o tijeras
 Juego de Piedra, papel o tijera que muestra el uso del módulo random y de 
 las estructura de datos Lista.
 El usuario selecciona alguna opción:  
-1. Piedra
-2. Papel
-3. Tijeras
+0. Piedra
+1. Papel
+2. Tijeras
 La computadora selecciona aleatoriamente y se muestra un mensaje con el 
 ganador.
--- day_4/main.py
+++ day_4/main.py
@@ -30,26 +30,26 @@ scissors = '''
 options = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(f"You: \n {options[player_choice - 1]}")
-computer_choice = random.randint(1, len(options))
-print(f"Computer: \n {options[computer_choice - 1]}")
+print(f"You: \n {options[player_choice]}")
+computer_choice = random.randint(0, 2)
+print(f"Computer: \n {options[computer_choice]}")
 
 if player_choice == computer_choice:
     print("Drawn! -_-")
-elif player_choice == 1:
-    if computer_choice == 2:
-        print("You Lose! :(")
-    elif computer_choice == 3:
-        print("You Win :)")
-elif player_choice == 2:
-    if computer_choice == 1:
-        print("You Win :)")
-    elif computer_choice == 3:
-        print("You Lose! :(")
-elif player_choice == 3:
+elif player_choice == 0:
     if computer_choice == 1:
         print("You Lose! :(")
     elif computer_choice == 2:
+        print("You Win :)")
+elif player_choice == 1:
+    if computer_choice == 0:
+        print("You Win :)")
+    elif computer_choice == 2:
+        print("You Lose! :(")
+elif player_choice == 2:
+    if computer_choice == 0:
+        print("You Lose! :(")
+    elif computer_choice == 1:
         print("You Win :)")
 else:
     print("Invalid Option. You lose :(")
